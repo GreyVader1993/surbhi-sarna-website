6bfbf8147bce827b68add5f4f176abe57aeb4f25
Tanmay Dureja <tanmaydureja@gmail.com>
2023-01-21 13:49:07 +0530

    Initialize Header and Footer

---
+++ src/pages/about.jsx
@@ -0,0 +1,12 @@
+// set an about page routed 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function About(){
+    return (
+        <div>
+            <h1>About</h1>
+        </div>
+    )
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// Path: src/pages/home.jsx
+
+// Compare this snippet from src/components/footer.jsx:
